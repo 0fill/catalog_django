31ce8af79d7bf7d6849a7c414726b6c26c86ae4c
0fill <fikrosek@seznam.cz>
2024-09-19 15:37:24 +0200

    login working

--- lets_read/catalog/urls.py
+++ lets_read/catalog/urls.py
@@ -1,16 +1,14 @@
 from django.urls import path
-from . import views
 from .views import *
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', home, name='home'),
     path('library/', BookListView.as_view(), name='library'),
     path('library/add_book', BookCreateView.as_view(), name='add_book'),
     path("book_detail/<int:pk>/", BookDetailView.as_view(), name='deatil_book'),
-    path("profile/", views.profile_page, name="profile"),
-    path('register/', views.register_page, name='register'),
+    path('accounts/profile/', profile_page, name="profile"),
+    path('register/', register_page, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
-
     ]

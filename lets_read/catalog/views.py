67925ebe136a416ab9fd4149619fcc7c05a7db10
0fill <fikrosek@seznam.cz>
2024-09-19 14:14:37 +0200

    done

--- lets_read/catalog/views.py
+++ lets_read/catalog/views.py
@@ -1,12 +1,30 @@
 from django.shortcuts import render, redirect
+from django.views.generic import ListView, DetailView, CreateView
+from django.urls import reverse_lazy
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.template import loader
 from .models import Book
-from .forms import BookForm
+from .forms import BookForm,UserRegistrationForm
 
 
 # Create your views here.
 
+class BookListView(ListView):
+    model = Book
+    template_name = 'all_books.html'
+
+
+class BookDetailView(DetailView):
+    model = Book
+    template_name = 'detail.html'
+
+
+class BookCreateView(CreateView):
+    model = Book
+    form_class = BookForm
+    template_name = 'add_book.html'
+    success_url = reverse_lazy('library')
 def home(request):
     return render(request, 'home.html')
 
@@ -30,3 +48,20 @@ def detail_book(request, name):
     book = Book.objects.get(name=name)
     context = {'book': book}
     return render(request, 'detail.html', context)
+
+@login_required
+def profile_page(request):
+    return render(request, 'profile.html')
+
+
+def register_page(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+
+    return render(request,
+                  'register.html',
+                  {'form': UserRegistrationForm()})
+
